7a5ef97e0248f9830cbdd2069952e4f43f6d2ee6
Urim Eom <vmffot1242@gmail.com>
2025-05-04 22:37:58 -0600

    built instance variables, constructors, getters

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -7,9 +7,8 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
-
-    private final int row;
-    private final int col;
+    private int row;
+    private int col;
 
     public ChessPosition(int row, int col) {
         this.row = row;
