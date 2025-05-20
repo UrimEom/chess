d75f12919352a5d3e4fc1b55c0dd132556d77b34
Urim Eom <vmffot1242@gmail.com>
2025-05-19 20:22:30 -0600

    created Service classes

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,13 @@
+package service;
+
+public class GameService {
+    public CreateResult createGame(CreateRequest createRequest) {
+
+    }
+    public JoinResult joinGame(JoinRequest joinRequest) {
+
+    }
+    public ListResult listGame(ListRequest listRequest) {
+
+    }
+}
