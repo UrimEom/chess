c33dacb4e97d4054d1efa4e4fdc86b5e3bd1fea6
Urim Eom <vmffot1242@gmail.com>
2025-05-20 19:38:11 -0600

    implemented LogoutHandler and revised error messages for logout

--- server/src/main/java/server/LogoutHandler.java
+++ server/src/main/java/server/LogoutHandler.java
@@ -1,4 +1,47 @@
 package server;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.AuthData;
+import service.UserService;
+import spark.Request;
+import spark.Response;
+
+import java.util.Map;
+
 public class LogoutHandler {
+    private final UserService userService;
+    private final Gson gson = new Gson();
+
+    public LogoutHandler(UserDAO userDAO, AuthDAO authDAO) {
+        this.userService = new UserService(userDAO, authDAO);
+    }
+
+    public String handler(Request req, Response res) {
+        try {
+            String authToken = req.headers("authorization");
+
+            if(authToken == null) {
+                res.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+            }
+
+            userService.logout(authToken);
+
+            res.status(200);
+            return "{}";
+
+        } catch (DataAccessException e) {
+            res.status(500);
+            return gson.toJson(Map.of("message", e.getMessage()));
+        } catch (Exception e) {
+            res.status(500);
+            return gson.toJson(Map.of("message", "Error: sever error"));
+        }
+
+    }
 }
