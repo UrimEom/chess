f0fd3270a74efc6633915c14332b452a591441f0
Urim Eom <vmffot1242@gmail.com>
2025-05-20 19:25:53 -0600

    implemented RegisterHandler

--- server/src/main/java/server/RegisterHandler.java
+++ server/src/main/java/server/RegisterHandler.java
@@ -1,5 +1,55 @@
 package server;
 
-public class RegisterHandler {
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.AuthData;
+import service.UserService;
+import spark.Request;
+import spark.Response;
 
+import java.util.Map;
+
+public class RegisterHandler {
+    private final UserService userService;
+    private final Gson gson = new Gson();
+
+    public RegisterHandler(UserDAO userDAO, AuthDAO authDAO) {
+        this.userService = new UserService(userDAO, authDAO);
+    }
+
+    public String handler(Request req, Response res) {
+        try {
+            JsonObject json = JsonParser.parseString(req.body()).getAsJsonObject();
+
+            String username = json.get("username").getAsString();
+            String password = json.get("password").getAsString();
+            String email = json.get("email").getAsString();
+
+            if(username == null || password == null || email == null) {
+                res.status(400);
+                return gson.toJson(Map.of("message", "Error: bad request"));
+            }
+
+            AuthData auth = userService.register(username, password, email);
+
+            res.status(200);
+            return gson.toJson(auth);
+
+        } catch (DataAccessException e) {
+            if (e.getMessage().equals("Error: bad request")) {
+                res.status(400);
+            } else if (e.getMessage().equals("Error: already taken")) {
+                res.status(403);
+            }
+            return gson.toJson(Map.of("Message", e.getMessage()));
+        } catch (Exception e) {
+            res.status(500);
+            return gson.toJson(Map.of("message", "Error: sever error"));
+        }
+
+    }
 }
