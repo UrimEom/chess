145c540d3c08f3efb78aebdae54c5fa9c480716c
Urim Eom <vmffot1242@gmail.com>
2025-05-20 20:20:41 -0600

    implemented ListHandler and revised some error messages in GameService

--- server/src/main/java/server/ListHandler.java
+++ server/src/main/java/server/ListHandler.java
@@ -1,4 +1,44 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.GameData;
+import service.GameService;
+import spark.Request;
+import spark.Response;
+
+import java.util.Collection;
+import java.util.Map;
+
 public class ListHandler {
+    private final GameService gameService;
+    private final Gson gson = new Gson();
+
+    public ListHandler(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameService = new GameService(gameDAO, authDAO);
+    }
+
+    public String handler(Request req, Response res) {
+        try {
+            String authToken = req.headers("authorization");
+            if(authToken == null) {
+                res.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+            }
+
+            Collection<GameData> games = gameService.listGames(authToken);
+
+            res.status(200);
+            return gson.toJson(Map.of("games", games));
+
+        } catch (DataAccessException e) {
+            res.status(500);
+            return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
+        } catch (Exception e) {
+            res.status(500);
+            return gson.toJson(Map.of("message", "Error: sever error"));
+        }
+    }
 }
--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -21,11 +21,11 @@ public class GameService {
     public GameData createGame(String gameName, String authToken) throws DataAccessException {
         AuthData auth = authDAO.getAuth(authToken);
         if(auth == null) {
-            throw new DataAccessException("Error: Invalid auth token");
+            throw new DataAccessException("Error: unauthorized");
         }
 
         if(gameName == null) {
-            throw new DataAccessException("Error: Game name is missing");
+            throw new DataAccessException("Error: bad request");
         }
 
         GameData newGame = new GameData(0, null, null, gameName, null);
@@ -37,12 +37,12 @@ public class GameService {
     public void joinGame(int gameID, String playerColor, String authToken) throws DataAccessException {
         AuthData auth = authDAO.getAuth(authToken);
         if(auth == null) {
-            throw new DataAccessException("Error: Invalid auth token");
+            throw new DataAccessException("Error: unauthorized");
         }
 
         GameData game = gameDAO.getGame(gameID);
         if(game == null) {
-            throw new DataAccessException("Error: Game is not found");
+            throw new DataAccessException("Error: bad request");
         }
 
         String username = auth.username();
@@ -51,16 +51,16 @@ public class GameService {
 
         if(playerColor.equalsIgnoreCase("WHITE")) {
             if(whitePlayer != null) {
-                throw new DataAccessException("Error: white user is already taken");
+                throw new DataAccessException("Error: already taken");
             }
             whitePlayer = username;
         }else if(playerColor.equalsIgnoreCase("BLACK")) {
             if(blackPlayer != null) {
-                throw new DataAccessException("Error: black user is already taken");
+                throw new DataAccessException("Error: already taken");
             }
             blackPlayer = username;
         }else {
-            throw new DataAccessException("Error: Invalid player color");
+            throw new DataAccessException("Error: Access Error");
         }
 
         GameData updatedData = new GameData(game.gameID(), whitePlayer, blackPlayer, game.gameName(), game.game());
@@ -71,7 +71,7 @@ public class GameService {
     public Collection<GameData> listGames(String authToken) throws DataAccessException {
         AuthData auth = authDAO.getAuth(authToken);
         if(auth == null) {
-            throw new DataAccessException("Error: Invalid auth token");
+            throw new DataAccessException("Error: unauthorized");
         }
 
         return gameDAO.listGames();
