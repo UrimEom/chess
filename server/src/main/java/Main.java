c3cd1f88a04248d0e7dad7b431330f841262b326
Urim Eom <vmffot1242@gmail.com>
2025-05-20 20:49:35 -0600

    created Server object for testing

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,17 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+
+        int port = 8080;
+        try {
+            Server server = new Server();
+            server.run(port);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
