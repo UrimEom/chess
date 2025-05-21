a217209ad045d83ac6c9bd9ed751b8bf38417e02
Urim Eom <vmffot1242@gmail.com>
2025-05-20 18:48:05 -0600

    implemented functions for GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,13 +1,80 @@
 package service;
 
+
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.AuthData;
+import model.GameData;
+
+import java.util.Collection;
+
 public class GameService {
-    public CreateResult createGame(CreateRequest createRequest) {
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
 
+    public GameService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
     }
-    public JoinResult joinGame(JoinRequest joinRequest) {
 
-    }
-    public ListResult listGame(ListRequest listRequest) {
+    public GameData createGame(String gameName, String authToken) throws DataAccessException {
+        AuthData auth = authDAO.getAuth(authToken);
+        if(auth == null) {
+            throw new DataAccessException("Error: Invalid auth token");
+        }
 
+        if(gameName == null) {
+            throw new DataAccessException("Error: Game name is missing");
+        }
+
+        GameData newGame = new GameData(0, null, null, gameName, null);
+        int gameID = gameDAO.createGame(newGame);
+
+        return gameDAO.getGame(gameID);
     }
+
+    public void joinGame(int gameID, String playerColor, String authToken) throws DataAccessException {
+        AuthData auth = authDAO.getAuth(authToken);
+        if(auth == null) {
+            throw new DataAccessException("Error: Invalid auth token");
+        }
+
+        GameData game = gameDAO.getGame(gameID);
+        if(game == null) {
+            throw new DataAccessException("Error: Game is not found");
+        }
+
+        String username = auth.username();
+        String whitePlayer = game.whiteUsername();
+        String blackPlayer = game.blackUsername();
+
+        if(playerColor.equalsIgnoreCase("WHITE")) {
+            if(whitePlayer != null) {
+                throw new DataAccessException("Error: white user is already taken");
+            }
+            whitePlayer = username;
+        }else if(playerColor.equalsIgnoreCase("BLACK")) {
+            if(blackPlayer != null) {
+                throw new DataAccessException("Error: black user is already taken");
+            }
+            blackPlayer = username;
+        }else {
+            throw new DataAccessException("Error: Invalid player color");
+        }
+
+        GameData updatedData = new GameData(game.gameID(), whitePlayer, blackPlayer, game.gameName(), game.game());
+
+        gameDAO.updateGame(updatedData);
+    }
+
+    public Collection<GameData> listGames(String authToken) throws DataAccessException {
+        AuthData auth = authDAO.getAuth(authToken);
+        if(auth == null) {
+            throw new DataAccessException("Error: Invalid auth token");
+        }
+
+        return gameDAO.listGames();
+    }
+
 }
