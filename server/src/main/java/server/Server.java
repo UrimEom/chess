7c0acb682a34b392b23e42434caf764d79be8df9
Urim Eom <vmffot1242@gmail.com>
2025-05-20 20:49:26 -0600

    implement Server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,9 @@
 package server;
 
+import dataaccess.*;
 import spark.*;
+
+import java.util.List;
 
 public class Server {
 
@@ -9,7 +12,17 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", new RegisterHandler(userDAO, authDAO)::handler);
+        Spark.post("/session", new LoginHandler(userDAO, authDAO)::handler);
+        Spark.delete("/session", new LogoutHandler(userDAO, authDAO)::handler);
+        Spark.get("/game", new ListHandler(gameDAO, authDAO)::handler);
+        Spark.post("/game", new CreateHandler(gameDAO, authDAO)::handler);
+        Spark.put("/game", new JoinHandler(gameDAO, authDAO)::handler);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
