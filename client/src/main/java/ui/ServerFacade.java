8fca4d4cb088e4d7f9e589792b1cbc7acad02f5b
Urim Eom <vmffot1242@gmail.com>
2025-05-30 11:21:29 -0600

    added methods needed for client

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,10 +1,17 @@
 package ui;
 
 import com.google.gson.Gson;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ServerFacade {
     private final String serverUrl;
@@ -15,14 +22,55 @@ public class ServerFacade {
     }
 
     //register
+    public AuthData register(String username, String password, String email) {
+        UserData user = new UserData(username, password, email);
+        return makeRequest("POST", "/user", user, AuthData.class, null);
+    }
+
     //login
+    public AuthData login(String username, String password) {
+        UserData user = new UserData(username, password, null);
+        return makeRequest("POST", "/session", user, AuthData.class, null);
+    }
+
     //logout
+    public void logout(String authToken) {
+        makeRequest("DELETE", "/session", null, Void.class, authToken);
+    }
 
     //create game
+    public GameData createGame(String gameName, String authToken) {
+        Map<String, String> request = Map.of("gameName", gameName);
+        return makeRequest("POST", "/game", request, GameData.class, authToken);
+    }
+
     //list game
+    public List<GameData> listGames(String authToken) {
+        GameData[] games = makeRequest("GET", "/game", null, GameData[].class, authToken);
+
+        if(games == null) {
+            return null;
+        }
+        return Arrays.asList(games);
+    }
+
     //join game
+    public void joinGame(int gameID, String playerColor, String authToken) {
+        Map<String, Object> request = new HashMap<>();
+        request.put("gameID", gameID);
+        request.put("playerColor", playerColor);
+        makeRequest("PUT", "/game", request, Void.class, authToken);
+    }
+
     //observe game
+    public void observeGame(int gameID, String authToken) {
+        joinGame(gameID, null, authToken);
+    }
+
     //print board
+    public void clearServer() {
+        makeRequest("DELETE", "/db", null, Void.class, null);
+    }
 
     private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String authToken) {
         try {
