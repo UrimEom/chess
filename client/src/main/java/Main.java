4cfda63c56ad460b479a118a34d3090da768112b
Urim Eom <vmffot1242@gmail.com>
2025-06-02 14:03:30 -0600

    added test for drawing board

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import chess.*;
+import ui.GameplayRepl;
 import ui.PreloginRepl;
 import ui.ServerFacade;
 
@@ -6,6 +7,12 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        //draw board
+        ChessGame game = new ChessGame();
+        ChessGame.TeamColor color = ChessGame.TeamColor.WHITE;
+        GameplayRepl repl = new GameplayRepl(game, color);
+        repl.drawBoard();
 
         String serverUrl = "http://localhost:8080";
 
