6e6a1118b218933a98add0c39a37d6b8077de8fb
Urim Eom <vmffot1242@gmail.com>
2025-06-02 14:02:52 -0600

    fixed printing board logic

--- client/src/main/java/ui/GameplayRepl.java
+++ client/src/main/java/ui/GameplayRepl.java
@@ -22,17 +22,15 @@ public class GameplayRepl {
         try {
             ChessBoard board = game.getBoard();
 
+            //color background
+            out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
             //draw headers
             out.print("   ");
-            if (color == ChessGame.TeamColor.BLACK) {
-                for (char c = 'h'; c >= 'a'; c--) {
-                    out.printf(" %c ", c);
-                }
-            } else {
-                for (char c = 'a'; c <= 'h'; c++) {
-                    out.printf(" %c ", c);
-                }
-            }
+            printLabel(out);
+
+            out.print("   ");
+            out.print(EscapeSequences.RESET_TEXT_COLOR);
+            out.print(EscapeSequences.RESET_BG_COLOR);
             out.println();
 
             //draw rows
@@ -46,31 +44,62 @@ public class GameplayRepl {
                 }
             }
 
+            out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+
             //draw footers
             out.print("   ");
-            if (color == ChessGame.TeamColor.BLACK) {
-                for (char c = 'h'; c >= 'a'; c--) {
-                    out.printf(" %c ", c);
-                }
-            } else {
-                for (char c = 'a'; c <= 'h'; c++) {
-                    out.printf(" %c ", c);
-                }
-            }
+            printLabel(out);
+
+            out.print("   ");
+            out.print(EscapeSequences.RESET_TEXT_COLOR);
+            out.print(EscapeSequences.RESET_BG_COLOR);
             out.println();
+
         }catch(Exception ex) {
             System.out.println("Display the board failed. Please try again.");
         }
     }
 
+    private void printLabel(PrintStream out) {
+        if (color == ChessGame.TeamColor.BLACK) {
+            for (char c = 'h'; c >= 'a'; c--) {
+                out.printf(" %c ", c);
+            }
+        } else {
+            for (char c = 'a'; c <= 'h'; c++) {
+                out.printf(" %c ", c);
+            }
+        }
+    }
     private void drawBoardRow(PrintStream out, ChessBoard board, int row, boolean isBlack) {
-        out.println(row + " ");
+        out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+        out.print(EscapeSequences.RESET_TEXT_COLOR);
+
+        out.print(" " + row + " ");
+        out.print(EscapeSequences.RESET_TEXT_COLOR);
+
         if(isBlack) {
             for(char col = 'h'; col >= 'a'; col--) {
                 ChessPosition pos = new ChessPosition(row, col - 'a' + 1);
                 drawSquare(out, board.getPiece(pos), row, col - 'a' + 1);
             }
+        }else {
+            for(char col = 'a'; col <= 'h'; col++) {
+                ChessPosition pos = new ChessPosition(row, col - 'a' + 1);
+                drawSquare(out, board.getPiece(pos), row, col - 'a' + 1);
+            }
         }
+        out.print(EscapeSequences.RESET_BG_COLOR);
+        out.print(EscapeSequences.RESET_TEXT_COLOR);
+
+        out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+        out.print(EscapeSequences.RESET_TEXT_COLOR);
+
+        out.print(" " + row + " ");
+
+        out.print(EscapeSequences.RESET_TEXT_COLOR);
+        out.print(EscapeSequences.RESET_BG_COLOR);
+        out.println();
     }
 
     private void drawSquare(PrintStream out, ChessPiece piece, int row, int col) {
@@ -78,17 +107,17 @@ public class GameplayRepl {
         if((row + col) % 2 == 1) {
             out.print(EscapeSequences.SET_BG_COLOR_BLACK);
         }else {
-            out.println(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+            out.print(EscapeSequences.SET_BG_COLOR_WHITE);
         }
 
         //color the piece
         if(piece == null) {
-            out.println("   ");
+            out.print("   ");
         }else {
             if(piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
-                out.println(EscapeSequences.SET_TEXT_COLOR_BLUE);
+                out.print(EscapeSequences.SET_TEXT_COLOR_BLUE);
             }else {
-                out.println(EscapeSequences.SET_TEXT_COLOR_RED);
+                out.print(EscapeSequences.SET_TEXT_COLOR_RED);
             }
 
             String symbol = switch (piece.getPieceType()) {
@@ -102,6 +131,5 @@ public class GameplayRepl {
             out.print(symbol);
             out.print(EscapeSequences.RESET_TEXT_COLOR);
         }
-        out.print(EscapeSequences.RESET_BG_COLOR);
     }
 }
