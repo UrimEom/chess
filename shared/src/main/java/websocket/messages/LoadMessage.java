35c26bac016bd8d57ecda13bedaf42509a524e61
Urim Eom <vmffot1242@gmail.com>
2025-06-05 17:57:20 -0600

    created subclass for load server message

---
+++ shared/src/main/java/websocket/messages/LoadMessage.java
@@ -0,0 +1,20 @@
+package websocket.messages;
+
+import chess.ChessGame;
+
+public class LoadMessage extends ServerMessage {
+    private ChessGame game;
+
+    public LoadMessage(ChessGame game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    public void setGame(ChessGame game) {
+        this.game = game;
+    }
+}
