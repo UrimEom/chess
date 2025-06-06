11c85d169e1977c905cc6e2f5d9863f205100e3b
Urim Eom <vmffot1242@gmail.com>
2025-06-06 12:34:56 -0600

    added functionalities from websocket

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,12 +1,16 @@
 package ui;
 
+import chess.ChessGame;
+import chess.ChessMove;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
+import websocket.commands.*;
 
+import javax.websocket.Session;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -19,100 +23,81 @@ import java.util.Map;
 public class ServerFacade {
     private final String serverUrl;
     private final Gson gson = new Gson();
+    private final HttpCommunicator http;
+    private WebsocketCommunicator ws;
+    private String authToken;
 
     public ServerFacade(String serverUrl) {
         this.serverUrl = serverUrl;
+        http = new HttpCommunicator(this, serverUrl);
     }
 
     //register
     public AuthData register(String username, String password, String email) {
-        UserData user = new UserData(username, password, email);
-        return makeRequest("POST", "/user", user, AuthData.class, null);
+        return http.register(username, password, email);
     }
 
     //login
     public AuthData login(String username, String password) {
-        UserData user = new UserData(username, password, null);
-        return makeRequest("POST", "/session", user, AuthData.class, null);
+        return http.login(username, password);
     }
 
     //logout
     public void logout(String authToken) {
-        makeRequest("DELETE", "/session", null, Void.class, authToken);
+        http.logout(authToken);
     }
 
     //create game
     public GameData createGame(String gameName, String authToken) {
-        Map<String, String> request = Map.of("gameName", gameName);
-        return makeRequest("POST", "/game", request, GameData.class, authToken);
+        return http.createGame(gameName, authToken);
     }
 
     //list game
     public List<GameData> listGames(String authToken) {
-        String response = makeRequest("GET", "/game", null, String.class, authToken);
-
-        JsonObject json = gson.fromJson(response, JsonObject.class);
-        JsonArray gameArray = json.getAsJsonArray("games");
-
-        GameData[] games = gson.fromJson(gameArray, GameData[].class);
-
-        return Arrays.asList(games);
+       return http.listGames(authToken);
     }
 
     //join game
     public void joinGame(int gameID, String playerColor, String authToken) {
-        Map<String, Object> request = new HashMap<>();
-        request.put("gameID", gameID);
-        if(playerColor != null) {
-            request.put("playerColor", playerColor);
-        }
-        makeRequest("PUT", "/game", request, Void.class, authToken);
+        http.joinGame(gameID, playerColor, authToken);
     }
 
-    //observe game
-
-    //print board
-    public void clearServer() {
-        makeRequest("DELETE", "/db", null, Void.class, null);
+    //join player
+    public void joinPlayer(int gameID, ChessGame.TeamColor color) {
+        UserGameCommand joinPlayerCommand = new JoinPlayerCommand(this.authToken, gameID, color);
+        this.ws.sendCommand(joinPlayerCommand);
     }
 
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String authToken) {
-        try {
-            URL url = new URL(serverUrl + path);
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setRequestMethod(method);
-            http.setRequestProperty("Accept", "application/json");
+    //join observer
+    public void joinObserver(int gameID) {
+        UserGameCommand joinObserverCommand = new JoinObserverCommand(this.authToken, gameID);
+        this.ws.sendCommand(joinObserverCommand);
+    }
 
-            if(authToken != null) {
-                http.setRequestProperty("Authorization", authToken);
-            }
+    //make move
+    public void makeMove(int gameID, ChessMove move) {
+        UserGameCommand makeMoveCommand = new MakeMoveCommand(this.authToken, gameID, move);
+        this.ws.sendCommand(makeMoveCommand);
+    }
 
-            if(request != null) {
-                http.setDoOutput(true);
-                http.setRequestProperty("Content-Type", "application/json");
-                try (OutputStream osReq = http.getOutputStream(); OutputStreamWriter writer = new OutputStreamWriter(osReq, StandardCharsets.UTF_8)) {
-                    gson.toJson(request, writer);
-                    writer.flush();
-                }
-            }
+    //leave game
+    public void leaveGame(int gameID) {
+        UserGameCommand leaveCommand = new LeaveCommand(this.authToken, gameID);
+        this.ws.sendCommand(leaveCommand);
+    }
 
-            if (http.getResponseCode() == 200) {
-                try (InputStream body = http.getInputStream(); InputStreamReader reader = new InputStreamReader(body)) {
-                    if(responseClass == String.class) {
-                        return responseClass.cast(new BufferedReader(reader).readLine());
-                    }else if (responseClass == Void.class) {
-                        return null;
-                    }
-                    return gson.fromJson(reader, responseClass);
-                }
-            }else {
-                try (InputStream body = http.getErrorStream(); InputStreamReader reader = new InputStreamReader(body)) {
-                    throw new RuntimeException(new BufferedReader(reader).readLine());
-                }
-            }
+    //resign game
+    public void resignGame(int gameID) {
+        UserGameCommand resignCommand = new ResignCommand(this.authToken, gameID);
+        this.ws.sendCommand(resignCommand);
+    }
 
-        }catch (IOException ex) {
-            throw new RuntimeException("HTTP request was failed: " + ex.getMessage());
-        }
+//    public void clearServer() {
+//        makeRequest("DELETE", "/db", null, Void.class, null);
+//    }
+
+    protected void setAuthToken(String authToken) {
+        this.authToken = authToken;
+        //maybe add exception?
     }
 }
