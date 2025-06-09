cf045d5757fcd61694830d2267f8be8109c7b1d7
Urim Eom <vmffot1242@gmail.com>
2025-06-09 16:09:39 -0600

    added toString()

--- shared/src/main/java/websocket/messages/ErrorMessage.java
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -15,4 +15,9 @@ public class ErrorMessage extends ServerMessage {
     public void setErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;
     }
+
+    @Override
+    public String toString() {
+        return errorMessage;
+    }
 }
