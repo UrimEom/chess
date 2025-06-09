fbfa693493a0f23307b1219340cac4b7a7e116af
Urim Eom <vmffot1242@gmail.com>
2025-06-09 14:05:22 -0600

    removed unnecessary testing code

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,4 @@
 import chess.*;
-import ui.GameplayRepl;
 import ui.PreloginRepl;
 import ui.ServerFacade;
 
@@ -7,12 +6,6 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-
-        //draw board
-        ChessGame game = new ChessGame();
-        ChessGame.TeamColor color = ChessGame.TeamColor.WHITE;
-        GameplayRepl repl = new GameplayRepl(game, color);
-        repl.drawBoard();
 
         String serverUrl = "http://localhost:8080";
 
