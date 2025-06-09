b9e51a567d7548fd29c6579d0ecbf1bdb58b1e51
Urim Eom <vmffot1242@gmail.com>
2025-06-09 12:12:15 -0600

    changed variable type for WebSocketTests

--- shared/src/main/java/websocket/messages/LoadMessage.java
+++ shared/src/main/java/websocket/messages/LoadMessage.java
@@ -1,20 +1,21 @@
 package websocket.messages;
 
 import chess.ChessGame;
+import model.GameData;
 
 public class LoadMessage extends ServerMessage {
-    private ChessGame game;
+    private GameData game;
 
-    public LoadMessage(ChessGame game) {
+    public LoadMessage(GameData game) {
         super(ServerMessageType.LOAD_GAME);
         this.game = game;
     }
 
-    public ChessGame getGame() {
+    public GameData getGame() {
         return game;
     }
 
-    public void setGame(ChessGame game) {
+    public void setGame(GameData game) {
         this.game = game;
     }
 }
