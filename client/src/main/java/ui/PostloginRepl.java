ba27ef824c883b521071b7e5846a31decbd638cf
Urim Eom <vmffot1242@gmail.com>
2025-06-09 18:29:27 -0600

    fixed error for rejoining

--- client/src/main/java/ui/PostloginRepl.java
+++ client/src/main/java/ui/PostloginRepl.java
@@ -84,7 +84,21 @@ public class PostloginRepl {
             return;
         }
 
-        int index = Integer.parseInt(inputs[1]) - 1;
+        doObserve(inputs[1]);
+    }
+
+    private void printHelp() {
+        out.println("create <NAME> -> to create a new game");
+        out.println("list -> to list existing games");
+        out.println("join <ID> [WHITE|BLACK] -> to join a game");
+        out.println("observe <ID> -> to observe a game");
+        out.println("logout -> to do when you are done");
+        out.println("quit -> to stop playing");
+        out.println("help -> to show possible commands");
+    }
+
+    private void doObserve(String input) {
+        int index = Integer.parseInt(input) - 1;
         if(index < 0 || index >= gameLists.size()) {
             System.out.println("Invalid game number");
             return;
@@ -97,16 +111,6 @@ public class PostloginRepl {
         ChessGame game = new ChessGame();
         GameplayRepl gameplayRepl = new GameplayRepl(server, gameData, game);
         gameplayRepl.run();
-    }
-
-    private void printHelp() {
-        out.println("create <NAME> -> to create a new game");
-        out.println("list -> to list existing games");
-        out.println("join <ID> [WHITE|BLACK] -> to join a game");
-        out.println("observe <ID> -> to observe a game");
-        out.println("logout -> to do when you are done");
-        out.println("quit -> to stop playing");
-        out.println("help -> to show possible commands");
     }
 
     private void doCreateGame(String name) {
@@ -156,26 +160,34 @@ public class PostloginRepl {
             GameData selected = gameLists.get(num);
             int gameID = selected.gameID();
             String authToken = auth.authToken();
+
             String current = auth.username();
 
-            if(teamColor.equals("WHITE")) {
-                if(!current.equals(selected.whiteUsername())) {
-                    System.out.println("You are not the WHITE player in the game.");
-                    return;
-                }
-            }else {
-                if(!current.equals(selected.blackUsername())) {
-                    System.out.println("You are not the BLACK player in the game.");
-                    return;
-                }
-                server.joinGame(gameID, "black", authToken);
+            String occupant = teamColor.equals("WHITE") ? selected.whiteUsername() : selected.blackUsername();
+
+            if(occupant != null && !occupant.equals(current)) {
+                System.out.println("That color is already taken.");
+                return;
             }
-            System.out.println("Joined game: " + (num + 1) + " as " + teamColor);
+
+
+            if(occupant == null) {
+                server.joinGame(gameID, teamColor, authToken);
+                System.out.println("Joined game: " + (num + 1) + " as " + teamColor);
+            }else {
+                out.printf("Re-joined game: %d as %s", num+1, teamColor);
+            }
+
             ChessGame.TeamColor correctColor = ChessGame.TeamColor.valueOf(teamColor);
-            new GameplayRepl(server , gameLists.get(gameID), new ChessGame(), correctColor).run();
+            new GameplayRepl(server, selected, new ChessGame(), correctColor).run();
 
         }catch (Exception ex) {
-            System.out.println("Invalid process. Please try again.");
+            if(ex.getMessage().contains("already taken")) {
+                System.out.println("That color is already taken. Please try again.");
+            }else {
+                ex.printStackTrace();
+                System.out.println("Invalid process. Please try again.");
+            }
         }
     }
 
